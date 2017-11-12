e5e3973e804b669000884e8d77d34ec8904df0b7
ed <mayday77@gmail.com>
2017-11-12 14:35:59 +0000

    minor changes

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mayday77@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures apache'
 long_description 'Installs/Configures apache'
-version '0.1.11'
+version '2.0.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -2,5 +2,11 @@
 
 file 'default www' do
   path '/var/www/html/index.html'
-  content 'helo there!'
+  content 'helo there! i am v2.0.0'
+end
+
+webnodes = search("node","role:web")
+
+webnodes.each do |each_node |
+	puts "DISPLAY node name: #{each_node}"
 end
