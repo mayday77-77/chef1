ed0da5d858870e60664bb433c8e757b00a07b062
ed <mayday77@gmail.com>
2017-11-12 12:50:54 +0000

    added new cookbook postgresql

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,19 +4,19 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-if node['platform_famliy'] == 'rhel'
-	package = "httpd"
+if node['platform_family'] == "rhel"
+	my_package = "httpd"
 elsif node['platform_family'] == "debian"
-	package = "apache2"
+	my_package = "apache2"
 end
 
 package 'apache2' do
-	package_name package
+	package_name my_package 
 	action :install
 end
 
 service 'apache2' do
-	service_name 'httpd'
+	service_name my_package 
 	action [:start,:enable]
 end
 
